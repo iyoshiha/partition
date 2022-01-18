c985d1f34fefc0d9b579d84f275e65b337a27657
iyoshiha <study.e.jp@gmail.com>
2022-01-19 08:26:10 +0900

    clear aizu tasks

--- partition.c
+++ partition.c
@@ -10,94 +10,58 @@ typedef struct	s_partition
 	int x;
 }				t_partition;
 
-void	swap_ary_elem(char *a_ptr, char *b_ptr)
+void	swap_ary_elem(int *a_ptr, int *b_ptr)
 {
 	int	tmp;
 
 	tmp = *a_ptr;
-	puts("~~~~~~~~~~~~~~~~~~~~~~");
-	puts("");
-		printf("in swap a: %d ", *a_ptr);
-	puts("");
-		printf("in swap b: %d ", *b_ptr);
-	puts("");
-	//*a_ptr = *a_ptr ^ *b_ptr;
 	*a_ptr = *b_ptr;
-		printf("in swap a: %d ", *a_ptr);
-	puts("");
-	//*b_ptr = *a_ptr ^ *b_ptr;
 	*b_ptr = tmp;
-		printf("in swap b: %d ", *b_ptr);
-	puts("");
-//	*a_ptr = *a_ptr ^ *b_ptr;
-	//	printf("in swap a: %d ", *a_ptr);
-	puts("");
-	puts("~~~~~~~~~~~~~~~~~~~~~~");
 }
 
-int	partition(char *ary, size_t p, size_t r)
+int	partition(int *ary, size_t p, size_t r)
 {
 	t_partition index;
 	int			pivot;
 
 	index.i = (int)p - 1;
-		printf("i: %d\n", index.i);
 	index.j = p;
-		printf("p: %d\n", index.j);
 	pivot = ary[r];
-		printf("pivot: %d\n", pivot);
-	for (int i = 0; i <= r ; i++)
-	{
-		printf("%d ", ary[i]);
-	}
-	puts("");
-	puts("in par() all elem done...");
 	while (index.j <= r)
 	{
 		if (ary[index.j] <= pivot)
 		{
 			index.i++;
-			printf("swap a: %d\n", ary[index.i]);
-			printf("swap b: %d\n", ary[index.j]);
 			swap_ary_elem(ary + index.i, ary + index.j);
 		}
 		index.j++;
-		puts("j++..");
-		puts("check ary elem...");
-	for (int i = 0; i <= r ; i++)
-	{
-		printf("%d ", ary[i]);
-	}
-		puts("done ...");
 	}
 	return (index.i);
 }
 
 int	main(int c, char **v)
 {
-	char ary[c - 1];
-	printf("%d\n\n", c);
+	int ary[c - 1];
+	int border;
 
 	c--;
 	v++;
-//  = {5,1,0,2,3,9,8,7,6,4};
 	for (int i = 0; i < c; i++)
 	{
 		ary[i] = atoi(v[i]);
 	}
-	puts("atoi done");
-	puts("checking elem in ary...");
-	for (int i = 0; i < c; i++)
+	border = partition(ary, 0, c - 1);
+	for (int i = 0; i < border; i++)
 	{
-		printf("%d ", ary[i]);
+		printf("%d", ary[i]);
+		printf(" ");
 	}
-	puts("");
-	puts("all elem done...");
-	partition(ary, 0, c - 1);
-	puts("~~~~partition~~~~");
-	for (int i = 0; i < c; i++)
+	printf("[%d] ", ary[border]);
+	for (int i = border + 1; i < c - 1; i++)
 	{
-		printf("%d ", ary[i]);
+		printf("%d", ary[i]);
+		printf(" ");
 	}
+	printf("%d", ary[c - 1]);
 	return (0);
 }
