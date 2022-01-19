7c964360abd95e5d7ca724e25f492f752a16f76a
iyoshiha <study.e.jp@gmail.com>
2022-01-19 09:39:54 +0900

    for aizu

--- partition.c
+++ partition.c
@@ -39,29 +39,31 @@ int	partition(int *ary, size_t start, size_t pivot_i)
 	return (index.i);
 }
 
-int	main(int c, char **v)
+int	main(void)
 {
-	int ary[c - 1];
+	int *ary;
 	int border;
+	int len;
+	int index;
 
-	c--;
-	v++;
-	for (int i = 0; i < c; i++)
-	{
-		ary[i] = atoi(v[i]);
-	}
-	border = partition(ary, 0, c -1);
+	scanf("%d", &len);
+	index = len -1;
+	ary = malloc(sizeof(int) * len);
+	for (int i = 0; i < len; i++)
+		scanf("%d", &ary[i]);
+	border = partition(ary, 0, index);
 	for (int i = 0; i < border; i++)
 	{
 		printf("%d", ary[i]);
 		printf(" ");
 	}
 	printf("[%d] ", ary[border]);
-	for (int i = border + 1; i < c - 1; i++)
+	for (int i = border + 1; i < index; i++)
 	{
 		printf("%d", ary[i]);
 		printf(" ");
 	}
-	printf("%d", ary[c - 1]);
+	printf("%d", ary[index]);
+	puts("");
 	return (0);
 }
